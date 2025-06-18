1d9bb622780b67d859ba5a389732c51491c19b55
komlan godwin amegah <komlan.godwin.amegah@gmail.com>
2025-06-18 14:56:36 +0200

    add notebook

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -1,13 +1,8 @@
 import os
-from threading import Thread
-import pandas as pd
-import numpy as np
 import zipfile
-from sklearn.model_selection import train_test_split
 import boto3
 from dotenv import load_dotenv
 from definition import DATASET_LOCAL_DIR
-from utils import generate_data
 
 # Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
@@ -58,31 +53,3 @@ if __name__ == "__main__":
 
     # (Optionnal) Remove zip file
     os.remove(local_zip_path)
-
-    data = pd.read_csv(f"{DATASET_LOCAL_DIR}/creditcard.csv", delimiter=',')
-
-    nonfrauds, frauds = data.groupby('Class').size()
-    print('Number of frauds: ', frauds)
-    print('Number of non-frauds: ', nonfrauds)
-    print('Percentage of fradulent data:', 100.*frauds/(frauds + nonfrauds))
-
-    feature_columns = data.columns[:-1]
-    label_column = data.columns[-1]
-
-    features = data[feature_columns].values.astype('float32')
-    labels = (data[label_column].values).astype('float32')
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        features, labels, test_size=0.1, random_state=42
-    )
-
-    print(f"X_test: {X_test[0]}")
-    if CONTINUE_SIMULATION:
-        thread = Thread(target = generate_data, args=[np.copy(X_test)])
-        thread.start()
-    else:
-        # one simulation at a time
-        generate_data(np.copy(X_test), max_requests=100)  # Adjust max_requests as needed
-
-
-
