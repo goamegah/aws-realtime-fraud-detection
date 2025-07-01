cf3194784d70b10a97f588a82775eeace1a7a55f
komlan godwin amegah <komlan.godwin.amegah@gmail.com>
2025-07-01 19:55:36 +0200

    remove scripts file

--- src/definition.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Define the configuration directory and file paths
-DATASET_LOCAL_DIR = os.path.join(Path(ROOT_DIR).parent.absolute(), 'dataset')
-# print(DATASET_DIR)
